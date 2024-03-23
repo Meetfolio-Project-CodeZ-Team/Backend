6047178a7f0522e31be3ffbf2123973dadb67ff9
gyeom <rlagusrua3687@gmail.com>
2024-03-23 16:22:29 +0900

    :tada: add project

---
+++ src/main/java/com/codez4/meetfolio/MeetfolioApplication.java
@@ -0,0 +1,13 @@
+package com.codez4.meetfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codez4/meetfolio/MeetfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codez4.meetfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
