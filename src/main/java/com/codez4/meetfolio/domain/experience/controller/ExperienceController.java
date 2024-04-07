69c18c399d46435e6df424ca8f6f086d943923c2
gyeom <rlagusrua3687@gmail.com>
2024-04-07 12:59:08 +0900

    :sparkles: Feat: 경험 분해 작성 Controller, post Request 구현
    Related: #16

--- src/main/java/com/codez4/meetfolio/domain/experience/controller/ExperienceController.java
+++ src/main/java/com/codez4/meetfolio/domain/experience/controller/ExperienceController.java
@@ -2,9 +2,13 @@ package com.codez4.meetfolio.domain.experience.controller;
 
 import static com.codez4.meetfolio.domain.experience.dto.ExperienceResponse.toExperienceResult;
 
+import com.codez4.meetfolio.domain.experience.dto.ExperienceRequest;
 import com.codez4.meetfolio.domain.experience.dto.ExperienceResponse.ExperienceInfo;
+import com.codez4.meetfolio.domain.experience.dto.ExperienceResponse.ExperienceProc;
 import com.codez4.meetfolio.domain.experience.dto.ExperienceResponse.ExperienceResult;
+import com.codez4.meetfolio.domain.experience.service.ExperienceCommandService;
 import com.codez4.meetfolio.domain.experience.service.ExperienceQueryService;
+import com.codez4.meetfolio.domain.member.Member;
 import com.codez4.meetfolio.domain.member.dto.MemberResponse.MemberInfo;
 import com.codez4.meetfolio.domain.member.service.MemberQueryService;
 import com.codez4.meetfolio.global.response.ApiResponse;
@@ -15,6 +19,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExperienceController {
 
     private final ExperienceQueryService experienceQueryService;
+    private final ExperienceCommandService experienceCommandService;
     private final MemberQueryService memberQueryService;
 
     @Operation(summary = "경험 분해 상세정보 조회", description = "특정 경험 분해 정보를 조회합니다.")
@@ -38,5 +45,15 @@ public class ExperienceController {
         ExperienceInfo experienceInfo = experienceQueryService.getExperience(experienceId);
 
         return ApiResponse.onSuccess(toExperienceResult(memberInfo, experienceInfo));
+    }
+
+    @Operation(summary = "경험 분해 작성", description = "경험 분해 작성 요청을 POST로 보냅니다.")
+    @PostMapping
+    public ApiResponse<ExperienceProc> post(@RequestBody ExperienceRequest.Post post) {
+
+        // TODO: 추후 로그인 사용자로 수정 - 1L
+        Member member = memberQueryService.findById(1L);
+
+        return ApiResponse.onSuccess(experienceCommandService.post(post, member));
     }
 }
---
+++ src/main/java/com/codez4/meetfolio/domain/experience/dto/ExperienceRequest.java
@@ -0,0 +1,63 @@
+package com.codez4.meetfolio.domain.experience.dto;
+
+import com.codez4.meetfolio.domain.enums.JobKeyword;
+import com.codez4.meetfolio.domain.experience.Experience;
+import com.codez4.meetfolio.domain.member.Member;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.time.LocalDate;
+import lombok.Getter;
+
+public class ExperienceRequest {
+
+    @Schema(description = "경험 분해 작성 Request")
+    @Getter
+    public static class Post {
+
+        @Schema(description = "경험 분해 제목")
+        private String title;
+
+        @Schema(description = "경험 분해 시작 기간", example = "2024-02-02")
+        private String startDate;
+
+        @Schema(description = "경험 분해 종료 기간", example = "2024-03-01")
+        private String endDate;
+
+        @Schema(description = "경험 카테고리")
+        private String experienceType;
+
+        @Schema(description = "경험 업무 내용")
+        private String task;
+
+        @Schema(description = "경험 동기 & 이유")
+        private String motivation;
+
+        @Schema(description = "맡았던 직무", example = "BACKEND")
+        private String jobKeyword;
+
+        @Schema(description = "사용한 기술 스택", example = "spring/mysql")
+        private String stack;
+
+        @Schema(description = "경험 세부 내용")
+        private String detail;
+
+        @Schema(description = "결과 및 성과")
+        private String advance;
+    }
+
+    public static Experience toEntity(Post reqeust, Member member) {
+
+        return Experience.builder()
+            .title(reqeust.getTitle())
+            .startDate(LocalDate.parse(reqeust.getStartDate()))
+            .endDate(LocalDate.parse(reqeust.getEndDate()))
+            .experienceType(reqeust.getExperienceType())
+            .task(reqeust.getTask())
+            .motivation(reqeust.getMotivation())
+            .jobKeyword(JobKeyword.valueOf(reqeust.getJobKeyword()))
+            .stack(reqeust.getStack())
+            .detail(reqeust.getDetail())
+            .advance(reqeust.getAdvance())
+            .member(member)
+            .build();
+    }
+}
