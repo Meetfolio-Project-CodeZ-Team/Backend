4bdf28d29745ae877ef6c89e0cde56d3b6e32619
gyeom <rlagusrua3687@gmail.com>
2024-04-07 15:09:40 +0900

    :sparkles: Feat: 경험 분해 목록 Paging Service 구현
    Related: #17

--- src/main/java/com/codez4/meetfolio/domain/experience/repository/ExperienceRepository.java
+++ src/main/java/com/codez4/meetfolio/domain/experience/repository/ExperienceRepository.java
@@ -1,8 +1,13 @@
 package com.codez4.meetfolio.domain.experience.repository;
 
 import com.codez4.meetfolio.domain.experience.Experience;
+import com.codez4.meetfolio.domain.member.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExperienceRepository extends JpaRepository<Experience, Long> {
+
+    Page<Experience> findAllByMember(Member member, Pageable pageable);
 
 }
