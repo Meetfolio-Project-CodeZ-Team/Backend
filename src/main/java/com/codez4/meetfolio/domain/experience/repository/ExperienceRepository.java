3cc22d262856d8d92a72a5cf1c978c9fb52d7c8d
gyeom <rlagusrua3687@gmail.com>
2024-04-08 23:16:57 +0900

    :sparkles: Feat: 로그인 여부에 따른 Experience Query 구현
    Related: #17

--- src/main/java/com/codez4/meetfolio/domain/experience/repository/ExperienceRepository.java
+++ src/main/java/com/codez4/meetfolio/domain/experience/repository/ExperienceRepository.java
@@ -1,7 +1,9 @@
 package com.codez4.meetfolio.domain.experience.repository;
 
+import com.codez4.meetfolio.domain.enums.JobKeyword;
 import com.codez4.meetfolio.domain.experience.Experience;
 import com.codez4.meetfolio.domain.member.Member;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +11,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ExperienceRepository extends JpaRepository<Experience, Long> {
 
     Page<Experience> findAllByMember(Member member, Pageable pageable);
+
+    List<Experience> findTop12ByJobKeywordOrderByIdDesc(JobKeyword jobKeyword);
+
+    List<Experience> findTop12ByOrderByIdDesc();
 
 }
