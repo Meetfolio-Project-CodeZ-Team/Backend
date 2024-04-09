e87e939bf5e927b49a1aedaf23e2458c415a1c54
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-04-09 13:46:31 +0900

    :memo: Feat: 회원가입 API Swagger 설정 추가

--- src/main/java/com/codez4/meetfolio/domain/member/controller/MemberController.java
+++ src/main/java/com/codez4/meetfolio/domain/member/controller/MemberController.java
@@ -3,15 +3,19 @@ package com.codez4.meetfolio.domain.member.controller;
 import com.codez4.meetfolio.domain.enums.Grade;
 import com.codez4.meetfolio.domain.enums.JobKeyword;
 import com.codez4.meetfolio.domain.enums.Major;
+import com.codez4.meetfolio.domain.member.Member;
 import com.codez4.meetfolio.domain.member.dto.LoginRequest;
 import com.codez4.meetfolio.domain.member.dto.MemberRequest;
 import com.codez4.meetfolio.domain.member.dto.MemberResponse;
 import com.codez4.meetfolio.domain.member.service.MemberCommandService;
 import com.codez4.meetfolio.domain.member.service.MemberQueryService;
+import com.codez4.meetfolio.global.annotation.AuthenticationMember;
 import com.codez4.meetfolio.global.jwt.JwtProperties;
 import com.codez4.meetfolio.global.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
@@ -28,7 +32,7 @@ public class MemberController {
 
     @Operation(summary = "회원 가입", description = "회원 가입")
     @PostMapping("/signup")
-    public ApiResponse<MemberResponse.MemberProc> signUp(@RequestBody MemberRequest.SignUpRequest request) {
+    public ApiResponse<MemberResponse.MemberProc> signUp(@Valid @RequestBody MemberRequest.SignUpRequest request) {
         MemberRequest.Post post = MemberRequest.Post.builder()
                 .email(request.getEmail())
                 .password(request.getPassword())
@@ -39,15 +43,13 @@ public class MemberController {
         return ApiResponse.onSuccess(memberCommandService.post(post));
     }
 
+    @Operation(summary = "로그인", description = "로그인")
     @PostMapping("/login")
-    public ResponseEntity login(@RequestBody LoginRequest request){
+    public ResponseEntity<ApiResponse<String>> login(@Valid @RequestBody LoginRequest request){
         String jwtToken = memberQueryService.login(request);
         HttpHeaders headers = new HttpHeaders();
         headers.add(JwtProperties.HEADER_STRING,JwtProperties.TOKEN_PREFIX + jwtToken);
-        return ResponseEntity.ok().headers(headers).body("로그인 성공");
+        return ResponseEntity.ok().headers(headers).body(ApiResponse.onSuccess("로그인에 성공하였습니다."));
     }
 
-//    @Operation(summary = "회원 정보 수정", description = "회원 정보 수정")
-//    @PatchMapping("/my-info")
-//    public ApiResponse<MemberResponse.MemberProc> updateMemberInfo()
 }
