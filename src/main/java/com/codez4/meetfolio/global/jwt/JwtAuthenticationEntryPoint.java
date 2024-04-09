931d8269431113c100fa2fe1da1987b42ed806fe
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-04-09 13:14:58 +0900

    :sparkles: Feat: JWT token generator 및 filter 구현

---
+++ src/main/java/com/codez4/meetfolio/global/jwt/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,39 @@
+package com.codez4.meetfolio.global.jwt;
+
+import com.codez4.meetfolio.global.response.code.ErrorReasonDto;
+import com.codez4.meetfolio.global.response.code.status.ErrorStatus;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void commence(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException
+    ) throws IOException {
+        log.error("Not Authenticated Request", authException);
+        log.error("Request Uri : {}", request.getRequestURI());
+        handleException(response);
+    }
+
+    private void handleException(HttpServletResponse response) throws IOException {
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("utf-8");
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.getWriter().write(objectMapper.writeValueAsString(ErrorReasonDto.toResponseEntity(ErrorStatus._UNAUTHORIZED)));
+    }
+}
---
+++ src/main/java/com/codez4/meetfolio/global/jwt/JwtExceptionFilter.java
@@ -0,0 +1,55 @@
+package com.codez4.meetfolio.global.jwt;
+
+import com.codez4.meetfolio.global.response.code.ErrorReasonDto;
+import com.codez4.meetfolio.global.response.code.status.ErrorStatus;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtExceptionFilter extends OncePerRequestFilter {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (JwtException jwtException) {
+            setErrorResponse(ErrorStatus._INVALID_TOKEN, response, jwtException);
+        } catch (UsernameNotFoundException usernameNotFoundException) {
+            setErrorResponse(ErrorStatus._MEMBER_NOT_FOUND, response, usernameNotFoundException);
+        }
+    }
+
+    public void setErrorResponse(
+            ErrorStatus status,
+            HttpServletResponse httpServletResponse,
+            Throwable throwable
+    ) throws IOException {
+        httpServletResponse.setStatus(HttpStatus.UNAUTHORIZED.value());
+        httpServletResponse.setContentType("application/json; charset=UTF-8");
+
+        ErrorReasonDto jwtExceptionResponse = ErrorReasonDto.builder()
+                .httpStatus(status.getHttpStatus())
+                .isSuccess(false)
+                .code(status.getCode())
+                .message(throwable.getMessage())
+                .build();
+
+        httpServletResponse.getWriter().write(objectMapper.writeValueAsString(jwtExceptionResponse));
+    }
+}
