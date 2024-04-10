f15fd70c31c590efb30f7b25f7d9dc525302be3c
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-04-10 22:47:09 +0900

    :fire: 사용되지 않는 import 삭제

--- src/main/java/com/codez4/meetfolio/domain/coverLetter/controller/CoverLetterController.java
+++ src/main/java/com/codez4/meetfolio/domain/coverLetter/controller/CoverLetterController.java
@@ -22,14 +22,7 @@ import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "자기소개서 API")
 @RestController
--- src/main/java/com/codez4/meetfolio/domain/experience/repository/ExperienceRepository.java
+++ src/main/java/com/codez4/meetfolio/domain/experience/repository/ExperienceRepository.java
@@ -3,10 +3,11 @@ package com.codez4.meetfolio.domain.experience.repository;
 import com.codez4.meetfolio.domain.enums.JobKeyword;
 import com.codez4.meetfolio.domain.experience.Experience;
 import com.codez4.meetfolio.domain.member.Member;
-import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface ExperienceRepository extends JpaRepository<Experience, Long> {
 
