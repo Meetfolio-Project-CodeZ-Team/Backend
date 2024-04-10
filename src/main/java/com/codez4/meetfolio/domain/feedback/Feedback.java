ddb0bcb1ac556e89ecfc108c0ac3ce1e90f3343f
gyeom <rlagusrua3687@gmail.com>
2024-04-10 18:31:47 +0900

    :sparkles: Feat: Feedback Soft-Delete 구현
    Related: #27

--- src/main/java/com/codez4/meetfolio/domain/feedback/Feedback.java
+++ src/main/java/com/codez4/meetfolio/domain/feedback/Feedback.java
@@ -2,19 +2,24 @@ package com.codez4.meetfolio.domain.feedback;
 
 import com.codez4.meetfolio.domain.common.BaseTimeEntity;
 import com.codez4.meetfolio.domain.coverLetter.CoverLetter;
+import com.codez4.meetfolio.domain.enums.Status;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.PostUpdate;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 
 @Getter
 @Entity
@@ -50,4 +55,16 @@ public class Feedback extends BaseTimeEntity {
 
     @Column(nullable = false)
     private Integer satisfaction;
+
+    @Column(nullable = false)
+    @ColumnDefault("'ACTIVE'")
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    /**
+     * update
+     */
+    public void delete() {
+        this.status = Status.INACTIVE;
+    }
 }
