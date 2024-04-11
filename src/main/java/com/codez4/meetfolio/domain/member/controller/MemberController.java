fb199650153655036e0636b361b83109b8bceb9e
gyeom <rlagusrua3687@gmail.com>
2024-04-11 17:16:05 +0900

    :sparkles: Feat: 마이페이지 사용자 정보 조회 API 구현
    Related: #35

--- src/main/java/com/codez4/meetfolio/domain/member/controller/MemberController.java
+++ src/main/java/com/codez4/meetfolio/domain/member/controller/MemberController.java
@@ -3,14 +3,19 @@ package com.codez4.meetfolio.domain.member.controller;
 import com.codez4.meetfolio.domain.enums.Grade;
 import com.codez4.meetfolio.domain.enums.JobKeyword;
 import com.codez4.meetfolio.domain.enums.Major;
+import com.codez4.meetfolio.domain.member.Member;
 import com.codez4.meetfolio.domain.member.dto.MemberRequest;
 import com.codez4.meetfolio.domain.member.dto.MemberResponse;
 import com.codez4.meetfolio.domain.member.service.MemberCommandService;
+import com.codez4.meetfolio.domain.member.service.MemberQueryService;
+import com.codez4.meetfolio.global.annotation.AuthenticationMember;
 import com.codez4.meetfolio.global.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     private final MemberCommandService memberCommandService;
+    private final MemberQueryService memberQueryService;
 
     @Operation(summary = "회원 가입", description = "회원 가입")
     @PostMapping("/signup")
@@ -35,6 +41,13 @@ public class MemberController {
                 .major(Major.convert(request.getMajor()))
                 .build();
         return ApiResponse.onSuccess(memberCommandService.post(post));
+    }
+
+    @Operation(summary = "마이페이지 수정 정보 조회", description = "사용자의 개인 정보를 조회합니다.")
+    @GetMapping("/mypage")
+    public ApiResponse<MemberResponse.MemberDetailInfo> myPage(@AuthenticationMember Member member) {
+
+        return ApiResponse.onSuccess(memberQueryService.getMyPage(member));
     }
 
 }
