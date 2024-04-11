15cdd8235e5ce95d12815ef194947f649f17d36f
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-04-11 16:01:03 +0900

    :sparkles: Feat: 사용자 포인트 충전 내역 Service 구현

--- src/main/java/com/codez4/meetfolio/domain/payment/repository/PaymentRepository.java
+++ src/main/java/com/codez4/meetfolio/domain/payment/repository/PaymentRepository.java
@@ -2,6 +2,7 @@ package com.codez4.meetfolio.domain.payment.repository;
 
 import com.codez4.meetfolio.domain.enums.Authority;
 import com.codez4.meetfolio.domain.enums.PointType;
+import com.codez4.meetfolio.domain.member.Member;
 import com.codez4.meetfolio.domain.payment.Payment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,6 +13,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
     Page<Payment> findByMember_Authority(Authority authority, Pageable pageable);
+
+    Page<Payment> findByMemberAndPoint_PointType(Member member, PointType point_pointType, Pageable pageable);
 
     @Query("SELECT IFNULL(MAX(PAYMENTSUM.POINT), 0) FROM (SELECT DATE_FORMAT(p.createdAt, '%Y-%c') AS MONTH, sum(p.payment) AS POINT FROM Payment p GROUP BY MONTH) AS PAYMENTSUM WHERE PAYMENTSUM.MONTH =:month")
     int queryGetTotalSales(String month);
