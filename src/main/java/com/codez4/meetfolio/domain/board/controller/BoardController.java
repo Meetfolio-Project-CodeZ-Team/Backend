2b9e9f3aca99ed414b70765258d08d553130f34e
gyeom <rlagusrua3687@gmail.com>
2024-04-13 22:30:29 +0900

    :bug: Fix: 마이페이지 관련 API MemberController 위치로 수정
    Related: #35

--- src/main/java/com/codez4/meetfolio/domain/board/controller/BoardController.java
+++ src/main/java/com/codez4/meetfolio/domain/board/controller/BoardController.java
@@ -1,40 +1,15 @@
 package com.codez4.meetfolio.domain.board.controller;
 
-import com.codez4.meetfolio.domain.board.dto.BoardResponse;
-import com.codez4.meetfolio.domain.board.dto.BoardResponse.BoardInfo;
-import com.codez4.meetfolio.domain.board.service.BoardQueryService;
-import com.codez4.meetfolio.domain.member.Member;
-import com.codez4.meetfolio.domain.member.dto.MemberResponse;
-import com.codez4.meetfolio.domain.member.dto.MemberResponse.MemberInfo;
-import com.codez4.meetfolio.global.annotation.AuthenticationMember;
-import com.codez4.meetfolio.global.response.ApiResponse;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "마이페이지 API")
+@Tag(name = "커뮤니티 게시글 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class BoardController {
 
-    private final BoardQueryService boardQueryService;
 
-    @Operation(summary = "내가 작성한 게시글 목록 조회", description = "내가 작성한 게시글 목록 조회 GET으로 보냅니다.")
-    @Parameter(name = "page", description = "페이징 번호, page, Query String입니다.", example = "0", in = ParameterIn.QUERY)
-    @GetMapping("/my-boards")
-    public ApiResponse<BoardResponse.MyBoardResult> getMyBoards(@AuthenticationMember Member member,
-        @RequestParam(name = "page") Integer page) {
-
-        MemberInfo memberInfo = MemberResponse.toMemberInfo(member);
-        BoardInfo boardInfo = boardQueryService.findMyBoards(member, page);
-
-        return ApiResponse.onSuccess(BoardResponse.toMyBoardResult(memberInfo, boardInfo));
-    }
 }
--- src/main/java/com/codez4/meetfolio/domain/member/controller/MemberController.java
+++ src/main/java/com/codez4/meetfolio/domain/member/controller/MemberController.java
@@ -1,16 +1,22 @@
 package com.codez4.meetfolio.domain.member.controller;
 
+import com.codez4.meetfolio.domain.board.dto.BoardResponse;
+import com.codez4.meetfolio.domain.board.dto.BoardResponse.BoardInfo;
+import com.codez4.meetfolio.domain.board.service.BoardQueryService;
 import com.codez4.meetfolio.domain.enums.Grade;
 import com.codez4.meetfolio.domain.enums.JobKeyword;
 import com.codez4.meetfolio.domain.enums.Major;
 import com.codez4.meetfolio.domain.member.Member;
 import com.codez4.meetfolio.domain.member.dto.MemberRequest;
 import com.codez4.meetfolio.domain.member.dto.MemberResponse;
+import com.codez4.meetfolio.domain.member.dto.MemberResponse.MemberInfo;
 import com.codez4.meetfolio.domain.member.service.MemberCommandService;
 import com.codez4.meetfolio.domain.member.service.MemberQueryService;
 import com.codez4.meetfolio.global.annotation.AuthenticationMember;
 import com.codez4.meetfolio.global.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +25,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "사용자 API")
@@ -29,6 +36,7 @@ public class MemberController {
 
     private final MemberCommandService memberCommandService;
     private final MemberQueryService memberQueryService;
+    private final BoardQueryService boardQueryService;
 
     @Operation(summary = "회원 가입", description = "회원 가입")
     @PostMapping("/signup")
@@ -58,6 +66,18 @@ public class MemberController {
         @RequestBody MemberRequest.Patch patch) {
 
         return ApiResponse.onSuccess(memberCommandService.update(member, patch));
+    }
+
+    @Operation(summary = "내가 작성한 게시글 목록 조회", description = "내가 작성한 게시글 목록 조회 GET으로 보냅니다.")
+    @Parameter(name = "page", description = "페이징 번호, page, Query String입니다.", example = "0", in = ParameterIn.QUERY)
+    @GetMapping("/my-boards")
+    public ApiResponse<BoardResponse.MyBoardResult> getMyBoards(@AuthenticationMember Member member,
+        @RequestParam(name = "page") Integer page) {
+
+        MemberInfo memberInfo = MemberResponse.toMemberInfo(member);
+        BoardInfo boardInfo = boardQueryService.findMyBoards(member, page);
+
+        return ApiResponse.onSuccess(BoardResponse.toMyBoardResult(memberInfo, boardInfo));
     }
 
 }
