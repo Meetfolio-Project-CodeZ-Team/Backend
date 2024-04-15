a1fbce3f03d9ad7f2ee272da753579b0af2cf6f9
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-04-15 11:32:13 +0900

    :sparkles: Feat: 관리자 AI 관리 Service 구현

---
+++ src/main/java/com/codez4/meetfolio/domain/model/repository/ModelRepository.java
@@ -0,0 +1,12 @@
+package com.codez4.meetfolio.domain.model.repository;
+
+import com.codez4.meetfolio.domain.model.Model;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ModelRepository extends JpaRepository<Model, Long> {
+    List<Model> findTop3ByOrderByAccuracy();
+}
