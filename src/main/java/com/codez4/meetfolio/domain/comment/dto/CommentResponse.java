63e064ffdec0d86a57b87c05fc4a1ea624f65b2e
gyeom <rlagusrua3687@gmail.com>
2024-04-16 22:28:15 +0900

    :sparkles: Feat: 댓글 DTO Swagger 적용
    Related: #46

--- src/main/java/com/codez4/meetfolio/domain/comment/dto/CommentResponse.java
+++ src/main/java/com/codez4/meetfolio/domain/comment/dto/CommentResponse.java
@@ -3,6 +3,7 @@ package com.codez4.meetfolio.domain.comment.dto;
 import com.codez4.meetfolio.domain.board.Board;
 import com.codez4.meetfolio.domain.comment.Comment;
 import com.codez4.meetfolio.domain.member.Member;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -11,13 +12,17 @@ import lombok.NoArgsConstructor;
 
 public class CommentResponse {
 
+    @Schema(description = "댓글 처리 응답 DTO")
     @Builder
     @AllArgsConstructor
     @NoArgsConstructor
     @Getter
     public static class CommentResult {
 
+        @Schema(description = "댓글 아이디")
         private Long commentId;
+
+        @Schema(description = "응답 생성 시간")
         private LocalDateTime createdAt;
     }
 
