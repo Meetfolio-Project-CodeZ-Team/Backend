338aa8e19c083ce1b1ea3e37bf255ea05de0aa82
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-04-18 19:48:57 +0900

    :sparkles: Feat: 로그인 / 로그아웃 API 구현

--- src/main/java/com/codez4/meetfolio/domain/member/controller/AuthController.java
+++ src/main/java/com/codez4/meetfolio/domain/member/controller/AuthController.java
@@ -1,20 +1,35 @@
 package com.codez4.meetfolio.domain.member.controller;
 
+import com.codez4.meetfolio.domain.member.Member;
 import com.codez4.meetfolio.domain.member.dto.LoginRequest;
+import com.codez4.meetfolio.domain.member.dto.LoginTokenResponse;
+import com.codez4.meetfolio.domain.member.dto.MemberResponse;
 import com.codez4.meetfolio.domain.member.service.MemberQueryService;
+import com.codez4.meetfolio.domain.member.service.AuthService;
+import com.codez4.meetfolio.global.annotation.AuthenticationMember;
+import com.codez4.meetfolio.global.jwt.JwtAuthenticationFilter;
 import com.codez4.meetfolio.global.jwt.JwtProperties;
+import com.codez4.meetfolio.global.jwt.JwtTokenProvider;
 import com.codez4.meetfolio.global.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.codez4.meetfolio.domain.member.dto.MemberResponse.toMemberInfo;
+
+@Slf4j
 @Tag(name = "로그인/로그아웃 API")
 @RestController
 @RequestMapping("/api")
@@ -22,14 +37,33 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final MemberQueryService memberQueryService;
+    private final AuthService authService;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    @Operation(summary = "로그인", description = "로그인")
+    @Operation(summary = "로그인", description = "로그인, 성공 시 response header access token과 refresh 토큰을, body에  로그인한 사용자 정보를 반환합니다.")
     @PostMapping("/login")
-    public ResponseEntity<ApiResponse<String>> login(@Valid @RequestBody LoginRequest request){
-        String jwtToken = memberQueryService.login(request);
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(JwtProperties.HEADER_STRING,JwtProperties.TOKEN_PREFIX + jwtToken);
-        return ResponseEntity.ok().headers(headers).body(ApiResponse.onSuccess("로그인에 성공하였습니다."));
+    public ResponseEntity<ApiResponse<MemberResponse.MemberInfo>> login(@Valid @RequestBody LoginRequest request,
+                                                                        HttpServletResponse response) {
+        Member member = memberQueryService.checkEmailAndPassword(request);
+        LoginTokenResponse tokenResponse = authService.authorize(member);
+        jwtTokenProvider.setHeaderAccessToken(response, tokenResponse.getAccessToken());
+        jwtTokenProvider.setHeaderRefreshToken(response, tokenResponse.getRefreshToken());
+        return ResponseEntity.ok().body(ApiResponse.onSuccess(toMemberInfo(member)));
+    }
+
+    @Operation(summary = "로그아웃", description = "로그아웃")
+    @PostMapping("/logout")
+    public String logout(@AuthenticationMember Member member,
+                         HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            response.setHeader(JwtProperties.ACCESS_HEADER_STRING, "");
+            String accessToken = JwtAuthenticationFilter.extractAccessToken(request);
+            String refreshToken = JwtAuthenticationFilter.extractRefreshToken(request);
+            authService.logout(accessToken, refreshToken);
+            ;
+        }
+        return "redirect:/main/index";
     }
 
 }
