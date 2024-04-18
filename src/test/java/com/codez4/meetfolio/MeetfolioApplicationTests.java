1cc1096185db57cbb047c0e8d9d26be5321adde4
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-04-18 19:21:17 +0900

    Update application-dev.yml

--- src/test/java/com/codez4/meetfolio/MeetfolioApplicationTests.java
+++ src/test/java/com/codez4/meetfolio/MeetfolioApplicationTests.java
@@ -1,13 +1,15 @@
 package com.codez4.meetfolio;
 
+import com.codez4.meetfolio.domain.emailAuth.controller.EmailAuthController;
+import com.codez4.meetfolio.domain.emailAuth.dto.EmailRequest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MeetfolioApplicationTests {
+@Autowired
+public EmailAuthController emailAuthController;
 
-	@Test
-	void contextLoads() {
-	}
 
 }
