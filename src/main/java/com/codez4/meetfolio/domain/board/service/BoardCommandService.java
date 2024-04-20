7d3bbfbba19f00538a5ba03e38aa8d32d0dbb72f
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-04-20 10:19:42 +0900

    :necktie: 게시물 수정 enum 검증 로직 추가

--- src/main/java/com/codez4/meetfolio/domain/board/service/BoardCommandService.java
+++ src/main/java/com/codez4/meetfolio/domain/board/service/BoardCommandService.java
@@ -7,7 +7,10 @@ import com.codez4.meetfolio.domain.board.dto.BoardRequest;
 import com.codez4.meetfolio.domain.board.repository.BoardRepository;
 import com.codez4.meetfolio.domain.board.repository.EmploymentBoardRepository;
 import com.codez4.meetfolio.domain.board.repository.GroupBoardRepository;
+import com.codez4.meetfolio.domain.enums.JobKeyword;
 import com.codez4.meetfolio.domain.member.Member;
+import com.codez4.meetfolio.global.exception.ApiException;
+import com.codez4.meetfolio.global.response.code.status.ErrorStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -31,15 +34,19 @@ public class BoardCommandService {
     }
 
     public void updateBoard(Board board, BoardRequest.BoardUpdate request) {
-        if (board instanceof EmploymentBoard employmentBoard){
-            employmentBoard.update(toEmploymentBoardPatch(request));
-        }
-        else if (board instanceof GroupBoard groupBoard){
-            groupBoard.update(toGroupBoardPatch(request));
+        if (board instanceof EmploymentBoard employmentBoard) {
+            if (JobKeyword.convert(request.getJobKeyword()) != null) {
+                employmentBoard.update(toEmploymentBoardPatch(request));
+            } else throw new ApiException(ErrorStatus._BAD_REQUEST);
+
+        } else if (board instanceof GroupBoard groupBoard) {
+            if (JobKeyword.convert(request.getGroupCategory()) != null) {
+                groupBoard.update(toGroupBoardPatch(request));
+            } else throw new ApiException(ErrorStatus._BAD_REQUEST);
         }
     }
 
-    public void deleteBoard(Board board){
+    public void deleteBoard(Board board) {
         boardRepository.delete(board);
     }
 
