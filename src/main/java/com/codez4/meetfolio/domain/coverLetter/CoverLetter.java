3ae1f1af4543cee6674ab4476b3e3b42c6718ded
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-04-22 20:43:23 +0900

    :bug: DynamicInsert 어노테이션 추가

--- src/main/java/com/codez4/meetfolio/domain/coverLetter/CoverLetter.java
+++ src/main/java/com/codez4/meetfolio/domain/coverLetter/CoverLetter.java
@@ -9,7 +9,9 @@ import com.codez4.meetfolio.domain.member.Member;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
 
+@DynamicInsert
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
