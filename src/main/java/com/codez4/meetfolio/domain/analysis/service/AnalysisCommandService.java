a8900cb4adb3372d839ac387a6e15739cbd70f4d
JOO WON SEO <joowojr@gmail.com>
2024-04-27 19:34:58 +0900

    🔀 Merge : Main Merge 14차 (#93)
    * Feat: 자기소개서 및 사용자 정보 API 구현 (#81)
    
    * :zap: feat: header에 필요한 로그인 사용자 정보 조회 API 추가
    
    Related: #80
    
    * :sparkles: feat: 사용자 이름을 통한 사용자 정보 조회 서비스 추가
    
    Related: #80
    
    * :sparkles: feat: 사용자 이름을 통한 다른 사용자 자기소개서 조회 API 구현
    
    Related: #80
    
    * :fire: fix: CoverLetter 작성, 수정 API 수정
    
    Related: #80
    
    * :fire: fix: 공개된 다른 사용자의 자기소개서 조회 API 수정
    
    Related: #80
    
    * :sparkles: feat: Security 경험 카드 조회 인증 허가
    
    Related: #81
    
    * :memo: fix: SecurityConfig 자료형 수정
    
    Related: #81
    
    * :fire: fix: 다른 사용자의 자기소개서 조회 시 필요한 DTO 생성
    
    Related: #81
    
    * :necktie: 로그아웃 로직 수정 (#84)
    
    * Fix: 대시보드 API 로직 수정 (#85)
    
    * :necktie: 대시보드 포인트 통계 산출 로직 수정
    
    * :necktie: 메소드 수정
    
    * :sparkles: Feat: 관리자 회원 검색 API 구현 (#86)
    
    * Refactor: 이메일 인증 코드 테이블 redis로 이관 (#88)
    
    * :recycle: 이메일 인증 코드 테이블 redis로 이관
    
    * :necktie: 인증코드 이메일 재전송 시 데이터 삭제 로직 추가
    
    * :necktie: 이메일 인증코드 데이터 삭제 시간 연장
    
    * 🔀 Fix: Coverletter API 오류 수정 (#92)
    
    * :fire: fix: 자기소개서 목록 조회시 softdelte 안된 데이터만 조회하도록 수정
    
    Related: #91
    
    * :fire: fix: 자기소개서 삭제시 연관 Feedback, Analysis NullException 해결
    
    Related: #91
    
    ---------
    
    Co-authored-by: kylo <103489352+kylo-dev@users.noreply.github.com>

--- src/main/java/com/codez4/meetfolio/domain/analysis/service/AnalysisCommandService.java
+++ src/main/java/com/codez4/meetfolio/domain/analysis/service/AnalysisCommandService.java
@@ -1,5 +1,6 @@
 package com.codez4.meetfolio.domain.analysis.service;
 
+import com.codez4.meetfolio.domain.analysis.Analysis;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,6 +13,6 @@ public class AnalysisCommandService {
     private final AnalysisQueryService analysisQueryService;
 
     public void softDelete(Long coverLetterId) {
-        analysisQueryService.findByCoverLetterId(coverLetterId).delete();
+        analysisQueryService.findByCoverLetterId(coverLetterId).ifPresent(Analysis::delete);
     }
 }
