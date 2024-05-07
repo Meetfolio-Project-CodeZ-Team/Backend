e680d85dc1900024d62a6649c7f78f1000ef9599
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-05-07 14:55:47 +0900

    :necktie: 카카오 페이 tid 조회 API 구현

--- src/main/java/com/codez4/meetfolio/domain/payment/repository/PaymentRepository.java
+++ src/main/java/com/codez4/meetfolio/domain/payment/repository/PaymentRepository.java
@@ -2,6 +2,7 @@ package com.codez4.meetfolio.domain.payment.repository;
 
 import com.codez4.meetfolio.domain.enums.Authority;
 import com.codez4.meetfolio.domain.enums.PaymentStatus;
+import com.codez4.meetfolio.domain.member.Member;
 import com.codez4.meetfolio.domain.payment.Payment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,6 +13,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
     Page<Payment> findByMember_AuthorityAndPaymentStatusIs(Authority authority, PaymentStatus paymentStatus, Pageable pageable);
+
+    Payment findTop1ByMember(Member member);
 
     @Query("SELECT IFNULL(SUM(p.payment),0) FROM Payment p WHERE p.paymentStatus = 'APPROVE'")
     long queryGetTotalSales();
