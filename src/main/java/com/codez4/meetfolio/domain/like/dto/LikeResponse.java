e35d3c32d0c0df5b3de55327ab557c66f5dcf3c1
JOO WON SEO <joowojr@gmail.com>
2024-05-15 20:05:16 +0900

    Feat: 좋아요 갯수 조회 API 구현 (#135)
    * :sparkles: Feat: 좋아요 갯수 조회 dto 생성
    
    * :sparkles: Feat: 좋아요 갯수 조회 Service 구현
    
    * :sparkles: Feat: 좋아요 갯수 조회 API 구현
    
    * :bug: 게시물 댓글 갯수 차감 로직 추가

--- src/main/java/com/codez4/meetfolio/domain/like/dto/LikeResponse.java
+++ src/main/java/com/codez4/meetfolio/domain/like/dto/LikeResponse.java
@@ -11,31 +11,61 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-@Schema(description = "좋아요 처리 응답 DTO")
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
 public class LikeResponse {
 
-    @Schema(description = "좋아요 상태", example = "ACTIVE / INACTIVE")
-    private String status;
 
-    @Schema(description = "응답 생성 시간")
-    private LocalDateTime createdAt;
+    @Schema(description = "좋아요 처리 응답 DTO")
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    public static class LikeResult {
+        @Schema(description = "게시물 ID")
+        private Long boardId;
 
-    public static LikeResponse toLikeResponse(Like like) {
+        @Schema(description = "게시물 좋아요 수")
+        private int likeCount;
 
-        return LikeResponse.builder()
-            .status(like.getStatus().toString())
-            .createdAt(LocalDateTime.now())
-            .build();
+        @Schema(description = "좋아요 상태", example = "ACTIVE / INACTIVE")
+        private String status;
+
+        @Schema(description = "응답 생성 시간")
+        private LocalDateTime createdAt;
+    }
+
+    @Schema(description = "좋아요 조화 응답 DTO")
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    public static class LikeCountResult{
+        @Schema(description = "게시물 ID")
+        private Long boardId;
+
+        @Schema(description = "게시물 좋아요 수")
+        private int likeCount;
+    }
+
+    public static LikeResult toLikeResult(Board board, Like like) {
+        return LikeResult.builder()
+                .boardId(board.getId())
+                .likeCount(board.getLikeCount())
+                .status(like.getStatus().toString())
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+
+    public static LikeCountResult toLikeCountResult(Board board){
+        return LikeCountResult.builder()
+                .boardId(board.getId())
+                .likeCount(board.getLikeCount())
+                .build();
     }
 
     public static Like toEntity(Member member, Board board) {
         return Like.builder()
-            .member(member)
-            .board(board)
-            .build();
+                .member(member)
+                .board(board)
+                .build();
     }
 }
