ee6ac2e51961249c23a4b7e3782b42e715a4fd54
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-05-15 19:59:58 +0900

    :sparkles: Feat: 좋아요 갯수 조회 API 구현

--- src/main/java/com/codez4/meetfolio/domain/like/controller/LikeController.java
+++ src/main/java/com/codez4/meetfolio/domain/like/controller/LikeController.java
@@ -1,7 +1,10 @@
 package com.codez4.meetfolio.domain.like.controller;
 
+import com.codez4.meetfolio.domain.board.Board;
+import com.codez4.meetfolio.domain.board.service.BoardQueryService;
 import com.codez4.meetfolio.domain.like.dto.LikeResponse;
 import com.codez4.meetfolio.domain.like.service.LikeCommandService;
+import com.codez4.meetfolio.domain.like.service.LikeQueryService;
 import com.codez4.meetfolio.domain.member.Member;
 import com.codez4.meetfolio.global.annotation.AuthenticationMember;
 import com.codez4.meetfolio.global.response.ApiResponse;
@@ -10,10 +13,8 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 @Tag(name = "좋아요 API")
 @RestController
 @RequiredArgsConstructor
@@ -21,13 +22,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class LikeController {
 
     private final LikeCommandService likeCommandService;
+    private final LikeQueryService likeQueryService;
+    private final BoardQueryService boardQueryService;
 
     @Operation(summary = "게시글 좋아요 요청", description = "게시글 좋아요 요청을 POST로 보냅니다.")
     @Parameter(name = "boardId", description = "게시글 Id, Path Variable입니다.", required = true, example = "1", in = ParameterIn.PATH)
     @PostMapping("/board-likes/{boardId}")
-    public ApiResponse<LikeResponse> likeProcess(@AuthenticationMember Member member,
-        @PathVariable(name = "boardId") Long boardId) {
+    public ApiResponse<LikeResponse.LikeResult> likeProcess(@AuthenticationMember Member member,
+                                                            @PathVariable(name = "boardId") Long boardId) {
 
         return ApiResponse.onSuccess(likeCommandService.changeLike(member, boardId));
     }
+
+    @Operation(summary = "게시글 좋아요 조회", description = "게시글의 좋아요 수를 조회합니다.")
+    @Parameter(name = "boardId", description = "게시글 Id, Path Variable입니다.", required = true, example = "1", in = ParameterIn.PATH)
+    @GetMapping("/board-likes/{boardId}")
+    public ApiResponse<LikeResponse.LikeCountResult> getLikeCount(@AuthenticationMember Member member,
+                                                                  @PathVariable(name = "boardId") Long boardId){
+        Board board  = boardQueryService.findById(boardId);
+        return ApiResponse.onSuccess(likeQueryService.getLikeCount(board));
+    }
+
+
 }
